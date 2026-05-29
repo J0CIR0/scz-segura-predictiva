3c7f99ad26166e4715e1afacda98879612ef700c
Josue <clarosrocajosue@gmail.com>
2026-05-29 16:03:14 -0400

    actualizando main: agregando funcionalies de reporte de incidentes·

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from backend.routes.auth_routes import router
+from backend.routes.auth_routes import router as auth_router
+from backend.routes.incidente_routes import router as incidente_router
 
-app = FastAPI(title="scz segura predictiva")
+app = FastAPI(title="SCZ Segura Predictiva")
 
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
-app.include_router(router, prefix="/api")
+
+app.include_router(auth_router, prefix="/api")
+app.include_router(incidente_router, prefix="/api")
 
 @app.get("/", response_class=HTMLResponse)
 def root():
